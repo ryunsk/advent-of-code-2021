6fb3d14e506ef3efbba8f92632dd1cf271ef13ca
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2021-12-12 18:26:45 +0000

    WIP: Problem 4

--- src/main/scala/problem4/Problem4.scala
+++ src/main/scala/problem4/Problem4.scala
@@ -1,5 +1,47 @@
 package problem4
 
+import scala.io.Source
+
 object Problem4 {
+  def solution(filePath: String): Unit = {
+    val inputData = readInput(filePath)
+    val numbersToDraw = parseInputToNumbersToDraw(inputData)
+    val bingoBoards = parseInputToAllBingoBoards(inputData)
+
+    //    println(numbersToDraw.mkString(","))
+    //    println(bingoBoards)
+    //    def loop():
+  }
+
+  private[problem4] def isWinner(board: Array[Array[String]]): Boolean = {
+    val horizontal = board.flatMap(_.mkString("")).mkString("")
+    val vertical = board.transpose.flatMap(_.mkString("")).mkString("")
+    horizontal.contains("XXXXX") || vertical.contains("XXXXX")
+  }
+
+  // Check if board has any winners
+  private[problem4] def parseInputToNumbersToDraw(input: Array[String]): Array[String] = {
+    input.head.split(",")
+  }
+
+  private[problem4] def parseInputToAllBingoBoards(input: Array[String]): Array[Array[Array[String]]] = {
+    // Array of 2D Boards (Array[Array[Int]])
+    input.tail.filter(_ != "").map(parseLineToRow).sliding(5, 5).toArray
+  }
+
+  private[problem4] def parseLineToRow(input: String): Array[String] = {
+    if (input.head.toString.equals(" ")) {
+      input.tail.replace("  ", " ").split(" ")
+    }
+    else {
+      input.replace("  ", " ").split(" ")
+    }
+  }
+
+  private[problem4] def readInput(filePath: String): Array[String] = {
+    val lines = Source.fromResource(filePath).getLines()
+    lines.toArray
+  }
+
 
 }
--- src/test/scala/problem4/Problem4Test.scala
+++ src/test/scala/problem4/Problem4Test.scala
@@ -4,8 +4,79 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Problem4Test extends AnyFlatSpec with Matchers {
-  it should "test" in {
-    1 + 1 should equal(2)
+  behavior of "file parser - input to numbers and board"
+  it should "parse random numbers correctly" in {
+    val filePath = "test-input-4"
+    val inputArray = Problem4.readInput(filePath)
+    Problem4.parseInputToNumbersToDraw(inputArray) should equal(
+      Array(7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1).map(_.toString)
+    )
+  }
+  it should "parse string line into array - 1" in {
+    val line = "22 13 17 11  0"
+    Problem4.parseLineToRow(line) should equal(Array("22", "13", "17", "11", "0"))
+  }
+  it should "parse string line into array - 2" in {
+    val line = " 8  2 23  4 24"
+    Problem4.parseLineToRow(line) should equal(Array("8", "2", "23", "4", "24"))
+  }
+
+  it should "parse all boards from input correctly" in {
+    val filePath = "test-input-4"
+    val inputArray = Problem4.readInput(filePath)
+    val allBoards = Problem4.parseInputToAllBingoBoards(inputArray)
+    val expectedBoard1 = Array(
+      Array(22, 13, 17, 11, 0),
+      Array(8, 2, 23, 4, 24),
+      Array(21, 9, 14, 16, 7),
+      Array(6, 10, 3, 18, 5),
+      Array(1, 12, 20, 15, 19)
+    ).map(_.map(_.toString))
+    val expectedBoard2 = Array(
+      Array(3, 15, 0, 2, 22),
+      Array(9, 18, 13, 17, 5),
+      Array(19, 8, 7, 25, 23),
+      Array(20, 11, 10, 24, 4),
+      Array(14, 21, 16, 12, 6)
+    ).map(_.map(_.toString))
+    val expectedBoard3 = Array(
+      Array(14, 21, 17, 24, 4),
+      Array(10, 16, 15, 9, 19),
+      Array(18, 8, 23, 26, 20),
+      Array(22, 11, 13, 6, 5),
+      Array(2, 0, 12, 3, 7)
+    ).map(_.map(_.toString))
+    val expectedResult = Array(expectedBoard1, expectedBoard2, expectedBoard3)
+
+    allBoards.head should equal(expectedResult.head)
+    allBoards(1) should equal(expectedResult(1))
+    allBoards(2) should equal(expectedResult(2))
+  }
+
+  it should "find winner - horizontal" in {
+    val winningBoard = Array(
+      Array("22", "13", "17", "11", "0"),
+      Array("8", "2", "23", "4", "24"),
+      Array("X", "X", "X", "X", "X"),
+      Array("6", "10", "3", "18", "5"),
+      Array("1", "12", "20", "15", "19")
+    )
+    Problem4.isWinner(winningBoard) should equal(true)
+  }
+  it should "find winner - vertical" in {
+    val winningBoard = Array(
+      Array("X", "13", "17", "11", "0"),
+      Array("X", "2", "23", "4", "24"),
+      Array("X", "9", "14", "16", "7"),
+      Array("X", "10", "3", "18", "5"),
+      Array("X", "12", "20", "15", "19")
+    )
+    Problem4.isWinner(winningBoard) should equal(true)
+  }
+
+  it should "find the correct solution" in {
+    val filePath = "test-input-4"
+    Problem4.solution(filePath)
   }
 
 }
