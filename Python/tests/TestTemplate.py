675afc0857ca9495fe168fe592972810552e06eb
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2021-12-19 21:58:31 +0000

    Test template

--- Python/tests/TestTemplate.py
+++ Python/tests/TestTemplate.py
@@ -4,10 +4,10 @@ import unittest
 class TestTemplate(unittest.TestCase):
 
     def test_template(self):
-        self.assertEquals([1, 2, 3], [1, 2, 3], "Should be the same")
-        self.assertEquals([[1, 2], [3, 4]], [[1, 2], [3, 4]], "Should be the same")
+        self.assertEqual([1, 2, 3], [1, 2, 3], "Should be the same")
+        self.assertEqual([[1, 2], [3, 4]], [[1, 2], [3, 4]], "Should be the same")
 
-        self.assertNotEquals([[1, 2], [3, 4]], [[1, 2], [1, 1]], "Should not be the same")
+        self.assertNotEqual([[1, 2], [3, 4]], [[1, 2], [1, 1]], "Should not be the same")
 
     def test_sum(self):
         assert sum([1, 2, 3]) == 6, "Should be 6"
